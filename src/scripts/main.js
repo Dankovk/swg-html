815ab4ef93e42a0854096cf2ccfb1189b3df8796
Dmitriy Kirillov <dmitriy.kirillov@popel.com.ua>
2015-09-23 17:25:31 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
+// VIEWPORT PLUGIN
+// $(":in-viewport") $(":below-the-fold") $(":above-the-top") $(":left-of-screen") $(":right-of-screen")
+
+(function($){$.belowthefold=function(element,settings){var fold=$(window).height()+$(window).scrollTop();return fold<=$(element).offset().top-settings.threshold;};$.abovethetop=function(element,settings){var top=$(window).scrollTop();return top>=$(element).offset().top+$(element).height()-settings.threshold;};$.rightofscreen=function(element,settings){var fold=$(window).width()+$(window).scrollLeft();return fold<=$(element).offset().left-settings.threshold;};$.leftofscreen=function(element,settings){var left=$(window).scrollLeft();return left>=$(element).offset().left+$(element).width()-settings.threshold;};$.inviewport=function(element,settings){return!$.rightofscreen(element,settings)&&!$.leftofscreen(element,settings)&&!$.belowthefold(element,settings)&&!$.abovethetop(element,settings);};$.extend($.expr[':'],{"below-the-fold":function(a,i,m){return $.belowthefold(a,{threshold:0});},"above-the-top":function(a,i,m){return $.abovethetop(a,{threshold:0});},"left-of-screen":function(a,i,m){return $.leftofscreen(a,{threshold:0});},"right-of-screen":function(a,i,m){return $.rightofscreen(a,{threshold:0});},"in-viewport":function(a,i,m){return $.inviewport(a,{threshold:0});}});})(jQuery);
+
 (function ($) {
 
     $('.selectize').selectize({
@@ -66,9 +71,11 @@ $(document).ready(function() {
        if (n == 3) n = 0;
    }, 3000);
 
-   var build1 = new Vivus('build_1', {type: 'scenario-sync', duration: 5, start: 'autostart', dashGap: 20, forceRender: false});
 });
 
 
 
 
+
+   var build1 = new Vivus('build_1', {type: 'scenario-sync', duration: 3, start: 'autostart', dashGap: 20, forceRender: false});
+   // $("svg:in-viewport").play(1);
